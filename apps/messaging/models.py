3e34b681dc2ae6640471beb9acc8dd4c441ce300
Popoola Temilorun <horluwatemilorunolamilekan@gmail.com>
2025-08-26 14:49:44 +0100

    include function for sending mail

--- apps/messaging/models.py
+++ apps/messaging/models.py
@@ -30,23 +30,35 @@ class Route(models.Model):
 
 
 class Message(models.Model):
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="messages")
-    route = models.ForeignKey(
-        Route, on_delete=models.CASCADE, related_name="messages")
-    payload = models.JSONField()
-    visitor_email = models.EmailField()
+
+    class Status(models.TextChoices):
+        QUEUED = "queued", "Queued"
+        SENT = "sent", "Sent"
+        FAILED = "failed", "Failed"
+
+
+    apikey = models.ForeignKey(
+        "key.APIKey", on_delete=models.CASCADE, related_name="messages"
+    )
+
+    recipient_email = models.EmailField()
+    visitor_email = models.EmailField()  # from contact form
+    subject = models.CharField(max_length=255, default="New Contact Message")
+    body = models.JSONField()
     accepted_at = models.DateTimeField(auto_now_add=True)
-    sent_at = models.DateTimeField(auto_now=True)
+    sent_at = models.DateTimeField(null=True, blank=True)
     status = models.CharField(
-        max_length=20, default="queued")  # queued/sent/failed
+        max_length=20, default="queued", choices=Status.choices
+    )
     error = models.TextField(blank=True)
+    preview_link = models.URLField(blank=True, null=True)
+    attachments = models.FileField(
+        upload_to="messages/attachments/", blank=True, null=True)
+
+    image_url = models.URLField(blank=True, null=True)
 
     class Meta:
-        ordering = ('-sent_at',)
+        ordering = ("-accepted_at",)
 
     def __str__(self):
-        return f' {self.status}: {self.route.recipient_email} - {self.visitor_email}'
-
-    def get_absolute_url(self):
-        return reverse("model_detail", kwargs={"pk": self.pk})
+        return f"{self.status.upper()} → {self.recipient_email} from {self.visitor_email}"
