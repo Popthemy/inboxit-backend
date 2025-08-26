4ceda5ee41823ad301fe87318208a382ca2a5949
Popoola Temilorun <horluwatemilorunolamilekan@gmail.com>
2025-08-26 14:49:17 +0100

    added endpoint for the userusage

--- apps/key/models.py
+++ apps/key/models.py
@@ -1,4 +1,3 @@
-import uuid
 import secrets
 from django.db import models
 from django.conf import settings
--- apps/key/views.py
+++ apps/key/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import get_object_or_404
 from django.http import Http404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import GenericAPIView
+from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import ApiKeySerializer, CreateApiKeySerializer
-from .models import APIKey
+from .serializers import ApiKeySerializer, CreateApiKeySerializer, UserUsageSerializer
+from .models import APIKey, UserUsage
 
 # Create your views here.
 
@@ -37,15 +38,18 @@ class ApiKeyView(GenericAPIView):
             data = {
                 'status': 'success',
                 'message': 'new api keys created successfully',
-                'api_key':raw_api_key,
+                'api_key': raw_api_key,
                 'data': serializer.data,
             }
             return Response(data=data, status=status.HTTP_201_CREATED)
-        return Response({'message':serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-
+        return Response({'message': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class RevokeApiKeyView(GenericAPIView):
+    '''
+    This endpoint is the details view and can also be used to revoke an api. 
+    When you revoke and api you can't receive message using the api
+    '''
     serializer_class = ApiKeySerializer
     permission_classes = [IsAuthenticated]
 
@@ -53,7 +57,7 @@ class RevokeApiKeyView(GenericAPIView):
 
         return get_object_or_404(APIKey, id=self.kwargs.get('pk'), user=self.request.user)
 
-    def get(self,*args, **kwargs):
+    def get(self, *args, **kwargs):
         try:
             key = self.get_object()
             serializer = self.get_serializer(key)
@@ -84,10 +88,19 @@ class RevokeApiKeyView(GenericAPIView):
                 'data': serializer.data,
             }
             return Response(data=data, status=status.HTTP_200_OK)
-        
+
         except Http404:
-            return Response({ 'status': 'error', 'message': 'API key not found'
-            }, status=status.HTTP_404_NOT_FOUND)
+            return Response({'status': 'error', 'message': 'API key not found'
+                             }, status=status.HTTP_404_NOT_FOUND)
         except Exception as e:
             return Response({'message': e}, status=status.HTTP_400_BAD_REQUEST)
 
+
+class UserUsageViewSet(ReadOnlyModelViewSet):
+    serializer_class = UserUsageSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user , _ =UserUsage.objects.get_or_create(user=self.request.user)
+        print(user)
+        return [user]
