34d83bb3b7eb5632d2853e33a9994b557fa26713
Popoola Temilorun <horluwatemilorunolamilekan@gmail.com>
2025-08-29 12:01:24 +0100

    repaired the migrations file of the account app

--- base/settings/development.py
+++ base/settings/development.py
@@ -6,6 +6,14 @@ load_dotenv()
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
 
 # email
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
--- base/settings/general.py
+++ base/settings/general.py
@@ -155,12 +155,6 @@ if DEBUG:
         "127.0.0.1"
     ]
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 
