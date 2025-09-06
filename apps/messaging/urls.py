4b38d4901ec7f245f1f23ca547279644092398b4
Popoola Temilorun <horluwatemilorunolamilekan@gmail.com>
2025-09-06 18:47:48 +0100

    updated documentation

--- apps/messaging/urls.py
+++ apps/messaging/urls.py
@@ -1,15 +1,15 @@
 from rest_framework.routers import DefaultRouter
-from .views import RouteViewSet, MessageViewSet, SendEmailWithApiKeyView,UserUsageViewSet
+from .views import RouteViewSet, MessageViewSet, SendEmailWithApiKeyView, UserUsageView
 from django.urls import path
 
 
 router = DefaultRouter()
 router.register("routes", RouteViewSet, basename="routes")
 router.register("messages", MessageViewSet, basename="messages")
-router.register('user-usage', UserUsageViewSet, basename='user-usage')
 
 
 urlpatterns = [
+  path('user-usage/', UserUsageView.as_view(), name='user-usage'),
   path("send-email/", SendEmailWithApiKeyView.as_view(), name="send-email"),
 
 ] + router.urls
