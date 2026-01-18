a84c95dd96b5dc6bc0e625b18e3589a05f355e56
Popoola Temilorun <horluwatemilorunolamilekan@gmail.com>
2026-01-18 18:36:10 +0100

    changed recipient email from  single email to alllow comma seperated email

--- apps/messaging/models.py
+++ apps/messaging/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.urls import reverse
 from django.core.validators import FileExtensionValidator
-from .validators import validate_file_size
+from .validators import validate_file_size, validate_email_list
 # Create your models here.
 
 User = settings.AUTH_USER_MODEL
@@ -19,15 +19,18 @@ class Route(models.Model):
         User, on_delete=models.CASCADE, related_name="routes")
     channel = models.CharField(max_length=20, choices=CHANNEL_CHOICES)
     is_active = models.BooleanField(default=True)
-    recipient_email = models.EmailField(
-        help_text='this email receives the message')
+    recipient_emails = models.TextField(
+        help_text='Comma separated emails that receive the message', validators=[validate_email_list])
+    # recipient_email = models.EmailField(
+    #     help_text='this email receives the message')
+
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ('-created_at', '-is_active')
 
     def __str__(self):
-        return f'{self.channel} - {self.recipient_email}'
+        return f'{self.channel} - {self.recipient_emails} ({ "Active" if self.is_active else "Inactive"})'
 
 
 class Message(models.Model):
@@ -41,7 +44,9 @@ class Message(models.Model):
         "key.APIKey", on_delete=models.CASCADE, related_name="messages"
     )
 
-    recipient_email = models.EmailField()
+    recipient_emails = models.TextField(
+        help_text='Comma separated emails that receive the message', validators=[validate_email_list])
+    # recipient_email = models.EmailField()
     visitor_email = models.EmailField()  # from contact form
     subject = models.CharField(max_length=255, default="New Contact Message")
     body = models.JSONField()
@@ -54,14 +59,13 @@ class Message(models.Model):
     attachments = models.FileField(
         upload_to="messages/attachments/", blank=True, null=True,
         validators=[validate_file_size, FileExtensionValidator(allowed_extensions=('pdf,doc'))])
-
     image_url = models.URLField(blank=True, null=True)
 
     class Meta:
         ordering = ("-accepted_at", '-sent_at')
 
     def __str__(self):
-        return f"{self.status.upper()} → {self.recipient_email} from {self.visitor_email}"
+        return f"{self.status.upper()} → {self.recipient_emails} from {self.visitor_email}"
 
     def get_absolute_url(self):
         return reverse("messages-detail", kwargs={"pk": self.pk})
