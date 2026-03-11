5faa3d532a5e5e1989ae0f4302304a921fa23e9f
Popoola Temilorun <horluwatemilorunolamilekan@gmail.com>
2026-03-11 10:44:13 +0100

    include static files configuratino

--- base/settings/general.py
+++ base/settings/general.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False')
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
 # Application definition
 
@@ -132,6 +132,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
